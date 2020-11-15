7225f3b3338ca233c7f89b0a04850ad3ffa79458
Perehodko <n.perehodko@yandex.com>
2020-11-15 16:38:39 +0300

    add files add files

--- framework/check.py
+++ framework/check.py
@@ -10,20 +10,6 @@ def _response_general_check(response, expected_code=codes.ok):
     except Exception as e:
         print("Исключение:", e)
 
-@allure.step
-def check_get_all_posts_response(response):
-    _response_general_check(response)
-    assert_that(len(response.json()), equal_to(100))
-
-@allure.step
-def check_get_title(response):
-    _response_general_check(response)
-    assert_that(response.json()["title"], equal_to("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"))
-
-@allure.step
-def check_get_id(response):
-    _response_general_check(response)
-    assert_that((response.status_code), equal_to(404))
 
 def _response_create_resourse(response, expected_code=201):
     try:
@@ -32,7 +18,26 @@ def _response_create_resourse(response, expected_code=201):
     except Exception as e:
         print("Исключение:", e)
 
+
+@allure.step
+def check_get_all_posts_response(response):
+    _response_general_check(response)
+    assert_that(len(response.json()), equal_to(100))
+
+
+@allure.step
+def check_get_title(response):
+    _response_general_check(response)
+    assert_that(response.json()["title"],
+                equal_to("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"))
+
+
+@allure.step
+def check_get_id(response):
+    _response_general_check(response)
+    assert_that((response.status_code), equal_to(404))
+
+
 @allure.step
 def check_create_resourse(response):
     _response_create_resourse(response)
-
