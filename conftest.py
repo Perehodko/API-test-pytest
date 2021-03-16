6d17ad764187b767ca234ec6b6f7fd1ba4a9d526
nperekhodko <nperekhodko@ozon.ru>
2021-03-16 15:01:20 +0300

    add parametrization to fixture

--- conftest.py
+++ conftest.py
@@ -11,17 +11,20 @@ def input_value():
 @pytest.fixture(scope='function')
 def prepared_resource_id(request, post_exist=None):
     response = get_all_post()
+    post_id= request.param
 
     try:
-        if response.json()[1]:
+        if response.json()[post_id]:
             post_exist = True
     except IndexError:
         post_exist = False
 
     if post_exist:
-        delete_post(1)
+        delete_post(post_id=request.param)
 
     def _clean_up():
         if not post_exist:
-            delete_post(1)
+            delete_post(post_id=request.param)
     request.addfinalizer(_clean_up)
+    return request.param
+
